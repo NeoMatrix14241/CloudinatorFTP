02c0eb197062cb62e52cf24c5413589fee1f6682
NeoMatrix14241 <kylefcapistrano@gmail.com>
2025-09-16 12:58:08 +0800

    Removed HTTP only option (Must work both HTTP and HTTPS)

--- app.py
+++ app.py
@@ -147,7 +147,6 @@ app.secret_key = SESSION_SECRET
 app.config.update(
     SESSION_COOKIE_SAMESITE='Lax',
     SESSION_COOKIE_SECURE=True,  # Enable secure cookies for Cloudflare Tunnel HTTPS
-    SESSION_COOKIE_HTTPONLY=True,
     PERMANENT_SESSION_LIFETIME=3600,  # 1 hour (in seconds)
     SESSION_REFRESH_EACH_REQUEST=True,
     SESSION_COOKIE_NAME='cloudinator_session'
