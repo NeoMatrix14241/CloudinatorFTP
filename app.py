0ee47f032def40cbe47debabd741c94b0be618b6
NeoMatrix14241 <kylefcapistrano@gmail.com>
2026-09-11 19:26:19 +0800

    Set SLOW_REQUEST_THRESHOLD_SECONDS to 0.5s

--- app.py
+++ app.py
@@ -578,7 +578,7 @@ app = Quart(__name__)
 # avoid spamming every normal request. Grep the log for "SLOW REQUEST" to
 # find recurrences.
 # ---------------------------------------------------------------------------
-SLOW_REQUEST_THRESHOLD_SECONDS = 2.0
+SLOW_REQUEST_THRESHOLD_SECONDS = 0.5
 
 
 @app.before_request
